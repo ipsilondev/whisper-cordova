36bf04192c05bbb9aa24d3dfb553039b4aeaa755
ipsilondev <david@ipsilondev.com>
2023-02-22 08:43:42 -0300

    first c++ version whisper

---
+++ www/WhisperCordova.js
@@ -0,0 +1,27 @@
+var WhisperCordova = function () {
+};
+
+WhisperCordova.DecodeChunkAudio = function (localImagePath, isBase64, fromTime, successCallback, failureCallback) {
+    if (typeof successCallback != 'function') {
+        throw new Error('DecodeChunkAudio Error: successCallback is not a function');
+    }
+
+    if (typeof failureCallback != 'function') {
+        throw new Error('DecodeChunkAudio Error: failureCallback is not a function');
+    }
+    if(isBase64 === true){
+    localImagePath = localImagePath.split(',')[1];
+	   }
+
+    return cordova.exec(
+        successCallback, failureCallback, 'WhisperCordova', 'decodeChunkAudio', [_getLocalImagePathWithoutPrefix(), isBase64, fromTime]);
+
+    function _getLocalImagePathWithoutPrefix() {
+        if (localImagePath.indexOf('file:///') === 0) {
+            return localImagePath.substring(7);
+        }
+        return localImagePath;
+    }
+};
+
+module.exports = WhisperCordova;
