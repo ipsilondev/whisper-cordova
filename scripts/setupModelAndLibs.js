59b462035cab5263048358da759aa476c7a89d6f
ipsilondev <david@ipsilondev.com>
2023-03-05 01:12:05 -0300

    fixing code and scripts

--- scripts/setupModelAndLibs.js
+++ scripts/setupModelAndLibs.js
@@ -10,6 +10,20 @@ function setupModelAndLibsiOS(ctx) {
 
 }
 
+function copyFileSync( source, target ) {
+
+    var targetFile = target;
+
+    // If target is a directory, a new file with the same name will be created
+    if ( fs.existsSync( target ) ) {
+        if ( fs.lstatSync( target ).isDirectory() ) {
+            targetFile = path.join( target, path.basename( source ) );
+        }
+    }
+
+    fs.writeFileSync(targetFile, fs.readFileSync(source));
+}
+
 function copyFolderRecursiveSync( source, target ) {
     var files = [];
 
