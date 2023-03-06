13efae6d1d55f7350a3f196bb6ea5b659b9d6d43
ipsilondev <david@ipsilondev.com>
2023-03-06 09:01:32 -0300

    removing unused code

--- android/WhisperCordova.java
+++ android/WhisperCordova.java
@@ -88,106 +88,6 @@ static {
         }
     }
 
-
-    /**
-     * Save image to device gallery
-     */
-    private void performImageSave() throws JSONException {
-        // create file from passed path
-        File srcFile = new File(filePath);
-
-        // destination gallery folder - external storage
-        File dstGalleryFolder = Environment.getExternalStoragePublicDirectory(Environment.DIRECTORY_PICTURES);
-
-        Log.d("whispercordova", "SaveImage dstGalleryFolder: " + dstGalleryFolder);
-
-        try {
-            // Create export file in destination folder (gallery)
-            File expFile = copyFile(srcFile, dstGalleryFolder);
-
-            // Update image gallery
-            scanPhoto(expFile);
-
-            callbackContext.success(expFile.toString());
-        } catch (RuntimeException e) {
-            callbackContext.error("RuntimeException occurred: " + e.getMessage());
-        }
-    }
-
-    /**
-     * Copy a file to a destination folder
-     *
-     * @param srcFile       Source file to be stored in destination folder
-     * @param dstFolder     Destination folder where to store file
-     * @return File         The newly generated file in destination folder
-     */
-    private File copyFile(File srcFile, File dstFolder) {
-        // if destination folder does not exist, create it
-        if (!dstFolder.exists()) {
-            if (!dstFolder.mkdir()) {
-                throw new RuntimeException("Destination folder does not exist and cannot be created.");
-            }
-        }
-
-        // Generate image file name using current date and time
-        String timeStamp = new SimpleDateFormat("yyyyMMdd_HHmmssSSS").format(new Date());
-        File newFile = new File(dstFolder.getPath() + File.separator + "IMG_" + timeStamp + ".jpg");
-
-        // Read and write image files
-        FileChannel inChannel = null;
-        FileChannel outChannel = null;
-
-        try {
-            inChannel = new FileInputStream(srcFile).getChannel();
-        } catch (FileNotFoundException e) {
-            throw new RuntimeException("Source file not found: " + srcFile + ", error: " + e.getMessage());
-        }
-        try {
-            outChannel = new FileOutputStream(newFile).getChannel();
-        } catch (FileNotFoundException e) {
-            throw new RuntimeException("Copy file not found: " + newFile + ", error: " + e.getMessage());
-        }
-
-        try {
-            inChannel.transferTo(0, inChannel.size(), outChannel);
-        } catch (IOException e) {
-            throw new RuntimeException("Error transfering file, error: " + e.getMessage());
-        } finally {
-            if (inChannel != null) {
-                try {
-                    inChannel.close();
-                } catch (IOException e) {
-                    Log.d("SaveImage", "Error closing input file channel: " + e.getMessage());
-                    // does not harm, do nothing
-                }
-            }
-            if (outChannel != null) {
-                try {
-                    outChannel.close();
-                } catch (IOException e) {
-                    Log.d("SaveImage", "Error closing output file channel: " + e.getMessage());
-                    // does not harm, do nothing
-                }
-            }
-        }
-
-        return newFile;
-    }
-
-    /**
-     * Invoke the system's media scanner to add your photo to the Media Provider's database,
-     * making it available in the Android Gallery application and to other apps.
-     *
-     * @param imageFile The image file to be scanned by the media scanner
-     */
-    private void scanPhoto(File imageFile) {
-        Intent mediaScanIntent = new Intent(Intent.ACTION_MEDIA_SCANNER_SCAN_FILE);
-        Uri contentUri = Uri.fromFile(imageFile);
-        mediaScanIntent.setData(contentUri);
-        cordova.getActivity().sendBroadcast(mediaScanIntent);
-    }
-
-
     /**
      * Callback from PermissionHelper.requestPermission method
      */
@@ -203,7 +103,7 @@ static {
 		switch (requestCode) {
 		case WRITE_PERM_REQUEST_CODE:
 			Log.d("SaveImage", "User granted the permission for WRITE_EXTERNAL_STORAGE");
-			performImageSave();
+			loadModelJNI(this.cordova.getActivity().getApplicationContext().getAssets(), this.filePath, this.isBase64, this.fromTime);
 			break;
 		}
 	}
